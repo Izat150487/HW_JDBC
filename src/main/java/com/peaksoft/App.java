5890bd2af7e6336e8dafb319dbb63b86bb1c337b
Izat150487 <104368950+Izat150487@users.noreply.github.com>
2022-12-29 17:41:59 +0300

    Initial commit

---
+++ src/main/java/com/peaksoft/App.java
@@ -0,0 +1,41 @@
+package com.peaksoft;
+
+import static com.peaksoft.db.DataBase.*;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main( String[] args ) {
+
+//        createTableCites();
+//        createTableCountry();
+//        createTableMayor();
+        // MAYOR TABLE
+//        insertTableMayor(1,"Алмаз Мамбетов ",50,"MAYOR");
+//        insertTableMayor(2,"Айбек Джунушалиев ",55,"MAYOR");
+//        insertTableMayor(3,"Нурбек Молдокадыров",53,"MAYOR");
+//        insertTableMayor(4,"Имамназар Бурканов",52,"MAYOR");
+//        insertTableMayor(5,"Азамат Арнабек уулу",60,"MAYOR");
+
+
+        //county
+//        insertCountryTable(1,"Kyrgyzstan");
+//        insertCountryTable(2,"Kazakhstan");
+//        insertCountryTable(3,"Uzbekistan");
+//        insertCountryTable(4,"Russian");
+//        insertCountryTable(5,"USA");
+        //cities
+//        insertCityTable(1,"Karakol");
+//        insertCityTable(2,"Bishkek");
+//        insertCityTable(3,"Naryn");
+//        insertCityTable(4,"Batken");
+//        insertCityTable(5,"Talas");
+
+        //getAllCountry
+        // getAllCountry();
+
+        //CallById city
+        //System.out.println(callByIdCities(1));
+    }
+}
